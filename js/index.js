1455739a794ace7293f3292e78bfead1fa5b3d01
msyfls123 <m18771058712@live.com>
2015-03-26 21:56:15 +0800

    3.26.21.56

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var menuArray=new Array(0,0,0,0,0,0,0,0,0,0);
 $(document).ready(function(){
 	$("#guide li").each(function(i){
 		var step=i*96;
-		<!--鼠标移入导航栏 -->
+		/*鼠标移入导航栏 */
 		$(this).mouseenter(function(){
 			$("#fly").stop().animate({left:step+"px"},500);
 			$(".menu ul:eq("+i+")").stop().slideDown(500);
@@ -14,7 +14,7 @@ $(document).ready(function(){
 				};
 			};
 		});
-		<!--鼠标移出导航栏 -->
+		/*鼠标移出导航栏 */
 		$(this).mouseleave(function(){
 			$("#con").mouseleave(function(){			
 				$(".menu ul:eq("+i+")").stop().slideUp(500,function(){menuArray[i]=0;});
@@ -22,7 +22,7 @@ $(document).ready(function(){
 			});
 		});
 	});
-
+	/*图片轮播*/
 	var t,imgT;
 	var Temp;
 	function ImgCircle(s,e){
@@ -36,10 +36,20 @@ $(document).ready(function(){
 		e++;
 		t=setTimeout(function(){ImgCircle(imgT,e)},5000)
 	};
-	ImgCircle(0,0);
 	$("#ban li").each(function(i){
 		$(this).click(function(){clearTimeout(t);ImgCircle(imgT,i);clearTimeout(t);});
 	});
-	$("#ban").mouseenter(function(){clearTimeout(t)});
+	$("#ban").mouseenter(function(){clearTimeout(t);clearTimeout(Temp)});
 	$("#ban").mouseleave(function(){Temp=setTimeout(function(){ImgCircle(imgT,imgT+1)},3000)});
+	ImgCircle(0,0);
+	/*新闻栏*/
+	$(".newsbox:eq(0) li").each(function(i){
+		var dis=60+35*i;
+		$(this).mouseenter(function(){
+			$("#slide").stop().animate({top:dis+"px"},500);
+		})
+	})
+	$(".newsbox:eq(0) ul").mouseleave(function(){
+			$("#slide").stop().animate({top:"60px"},500);
+	})
 })
