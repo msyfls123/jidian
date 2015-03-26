1dd9a6279bdbc3df3e9c6f1810aa647a440b9602
msyfls123 <m18771058712@live.com>
2015-03-26 19:27:36 +0800

    3.26.19.27

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ var menuArray=new Array(0,0,0,0,0,0,0,0,0,0);
 $(document).ready(function(){
 	$("#guide li").each(function(i){
 		var step=i*96;
+		<!--鼠标移入导航栏 -->
 		$(this).mouseenter(function(){
 			$("#fly").stop().animate({left:step+"px"},500);
 			$(".menu ul:eq("+i+")").stop().slideDown(500);
@@ -13,15 +14,14 @@ $(document).ready(function(){
 				};
 			};
 		});
+		<!--鼠标移出导航栏 -->
 		$(this).mouseleave(function(){
-			$(".menu").mouseleave(function(){			
+			$("#con").mouseleave(function(){			
 				$(".menu ul:eq("+i+")").stop().slideUp(500,function(){menuArray[i]=0;});
+				$("#fly").stop().animate({left:"0px"},500);
 			});
 		});
 	});
-	$("#guide").mouseleave(function(){
-		$(".menu").mouseleave(function(){
-			$("#fly").stop().animate({left:"0px"},500)
-		})
-	})
+
+
 })
