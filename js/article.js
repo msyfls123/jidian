5b59ac245cec6879d1a753c3c686fe1e67065990
msyfls123 <m18771058712@live.com>
2015-05-02 21:25:14 +0800

    5.2.21.25

--- js/article.js
+++ js/article.js
@@ -81,7 +81,7 @@ $(document).ready(function(){
 		copy: getText(),
 		afterCopy: function(){
 			$("#reminder>span:nth-child(2)").css("color",'#ccc');
-			$("#reminder span:eq(1)").after($("<span id='msg'></span>")).text("复制成功").fadeOut(2000);
+			$("#reminder span:eq(1)").after($("<ad id='msg'></ad>")).text("复制成功").fadeOut(2000);
 		}
 	});
 });
