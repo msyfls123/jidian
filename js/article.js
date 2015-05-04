d317b7f947f44bf7174732bfb29a4afbdb844e71
msyfls123 <m18771058712@live.com>
2015-05-04 12:27:00 +0800

    5.4.12.26

--- js/article.js
+++ js/article.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 	};
 	//页面滚动固定标题和左侧导航栏
 		var navH = $("#nav").offset().top;//获取要定位元素距离浏览器顶部的距离
-		var titleH= $("#title").offset().top;//获取要定位元素距离浏览器顶部的距离
+		//var titleH= $("#title").offset().top;//获取要定位元素距离浏览器顶部的距离
 		function scrollEvent1(){            //固定导航栏
 			var scroH = $(this).scrollTop();
 			if(scroH>=navH-30){
@@ -58,15 +58,15 @@ $(document).ready(function(){
 				$("#nav").css({"position":"absolute","top":"40px","left":"0","margin-left":"0",});
 			}
 		};
-		function scrollEvent2(){            //固定标题
+		/*function scrollEvent2(){            //固定标题
 			var scroH = $(this).scrollTop();
 			if(scroH>=navH-30){
 				$("#title").css({"position":"fixed","top":"20px","left":"50%","margin-left":"-300px","background":"#fff"});
 			}else if(scroH<navH-30){
 				$("#title").css({"position":"absolute","top":"10px","left":"180px","margin-left":"0px","background":"none"});
 			}
-		};
-		$(window).scroll(function(){scrollEvent1();scrollEvent2();scrollEvent3();});   //注册固定导航栏事件
+		};*/
+		$(window).scroll(function(){scrollEvent1();/*scrollEvent2()*/;scrollEvent3();});   //注册固定导航栏事件
 
 		$("#backTop").click(function(){ 
 			$('body,html').animate({scrollTop:"0px"}, 1000); return false;
